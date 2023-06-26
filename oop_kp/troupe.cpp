f6a46daff911ab45d6024cee78bdb55a1f230f36
Alena Kasatkina <100072041+wogusfer@users.noreply.github.com>
2023-06-26 19:12:49 +0300

    Create troupe.cpp

---
+++ oop_kp/troupe.cpp
@@ -0,0 +1,21 @@
+#include "troupe.h"
+
+Troupe::Troupe()
+{
+
+}
+
+Troupe::~Troupe()
+{
+
+}
+
+int Troupe::getId() const
+{
+    return id;
+}
+
+void Troupe::setId(int newId)
+{
+    id = newId;
+}
